e26642bf93616ecdbef7995b07ec8782e2c61acb
Pcosby5 <pcosby50@gmail.com>
2023-10-03 16:34:08 +0000

    8-uppercase.py

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def uppercase(str):
     for lower_case in str:
-        if ord(lower_case) > 96 and ord(lower_case) < 123: #check for lowercase using ASCII
-            lower_case = chr(ord(lower_case) - 32) #converts lowercase to uppercase
+        if ord(lower_case) > 96 and ord(lower_case) < 123:
+            lower_case = chr(ord(lower_case) - 32)
         print("{}".format(lower_case), end="")
     print("")
