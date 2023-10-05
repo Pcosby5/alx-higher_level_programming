d7a568c8e57c8f7ebd74a1a2a91824f063fb1c69
Pcosby5 <pcosby50@gmail.com>
2023-10-05 11:39:01 +0000

    1-calculation.py

---
+++ 0x02-python-import_modules/1-calculation.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+# import function from python file
+from calculator_1 import add, sub, mul, div
+# does not execute upon importing
+if __name__ == "__main__":
+    a = 10
+    b = 5
+    print("{} + {} = {}".format(a, b, add(a, b)))
+    print("{} - {} = {}".format(a, b, sub(a, b)))
+    print("{} * {} = {}".format(a, b, mul(a, b)))
+    print("{} / {} = {}".format(a, b, div(a, b)))
