dc986836f314086ad3a72153942ad380d9d9ee0a
Pcosby5 <pcosby50@gmail.com>
2023-10-05 12:24:57 +0000

    1-calculation.py

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,9 +1,5 @@
 #!/usr/bin/python3
-# import function from python file
-
 from calculator_1 import add, sub, mul, div
-# does not execute upon importing
-
 if __name__ == "__main__":
     a = 10
     b = 5
@@ -14,9 +10,6 @@ if __name__ == "__main__":
     division = a / b
 
     print("{} + {} = {}".format(a, b, addition))
-
     print("{} - {} = {}".format(a, b, subtraction))
-
     print("{} * {} = {}".format(a, b, multiplication))
-
     print("{} / {} = {}".format(a, b, division))
