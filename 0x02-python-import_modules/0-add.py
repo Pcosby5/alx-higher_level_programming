78dcf1a3d1df2cf98dfe5e7072ee45e0a4990c41
Pcosby5 <pcosby50@gmail.com>
2023-10-05 12:59:29 +0000

    0-add.py

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -3,5 +3,5 @@ a = 1
 b = 2
 if __name__ == "__main__":
     from add_0 import add
-outcome = add(a, b)
-print("{:d} + {:d} = {:d}".format(a, b, outcome))
+addition = add(a, b)
+print("{:d} + {:d} = {:d}".format(a, b, addition))
