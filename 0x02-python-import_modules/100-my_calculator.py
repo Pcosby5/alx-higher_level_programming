9a8ebcab863fda41c2799f22ac2c2325b15d7a3c
Pcosby5 <pcosby50@gmail.com>
2023-10-05 23:47:04 +0000

    100-my_calculator.py

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,29 +1,27 @@
 #!/usr/bin/python3
 from calculator_1 import add, sub, mul, div
-import sys
+from sys import argv
 
 if __name__ == "__main__":
-    if len(sys.argv) != 4:
+    if len(argv) != 4:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
-        sys.exit(1)
+        exit(1)
 
-    a = int(sys.argv[1])
-    operator = sys.argv[2]
-    b = int(sys.argv[3])
+    a = int(argv[1])
+    b = int(argv[3])
 
-    if operator == '+':
+    if argv[2] == '+':
         addition = add(a, b)
-    elif operator == '-':
+        print("{} + {} = {}".format(a, b, addition))
+    elif argv[2] == '-':
         subtraction = sub(a, b)
-    elif operator == '*':
+        print("{} - {} = {}".format(a, b, subtraction))
+    elif argv[2] == '*':
         multiplication = mul(a, b)
-    elif operator == '/':
+        print("{} * {} = {}".format(a, b, multiplication))
+    elif argv[2] == '/':
         division = div(a, b)
+        print("{} / {} = {}".format(a, b, division))
     else:
         print("Unknown operator. Available operators: +, -, * and /")
-        sys.exit(1)
-
-    print("{} {} {} = {}".format(a, operator, b, addition))
-    print("{} {} {} = {}".format(a, operator, b, subtraction))
-    print("{} {} {} = {}".format(a, operator, b, multiplication))
-    print("{} {} {} = {}".format(a, operator, b, division))
+        exit(1)
