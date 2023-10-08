9ab143f15b65e6f2e385fd7c9051b674874712b7
Pcosby5 <pcosby50@gmail.com>
2023-10-08 20:56:38 +0000

    7-add_tuple.py

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,11 +1,22 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
     # Ensure both tuples have at least 2elements
-    tuple_a = tuple_a + (0, 0)
-    tuple_b = tuple_b + (0, 0)
+    if len(tuple_a) == 0:
+        tuple_a = (0, 0)
+    elif len(tuple_a) == 1:
+        tuple_a = (tuple_a[0], 0)
+    else:
+        tuple_a = tuple_a
+
+    if len(tuple_b) == 0:
+        tuple_b = (0, 0)
+    elif len(tuple_b) == 1:
+        tuple_b = (tuple_b[0], 0)
+    else:
+        tuple_b = tuple_b
+
     # Add first index of each tuple
     tuple_c = tuple_a[0] + tuple_b[0]
     # Add the second index of each tuple
     tuple_d = tuple_a[1] + tuple_b[1]
     return (tuple_c, tuple_d)
-
