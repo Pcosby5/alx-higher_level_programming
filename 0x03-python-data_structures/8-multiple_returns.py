437943a3b814157304d2edfc0b1cb601b72d35f1
Pcosby5 <pcosby50@gmail.com>
2023-10-08 21:17:17 +0000

    8-multiple_returns.py

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-    length_sentence = len(sentence)
-    first_character = sentence[0]
     # check if string is empty
     if not sentence:
         return (0, None)
+    length_sentence = len(sentence)
+    first_character = sentence[0]
     return (length_sentence, first_character)
