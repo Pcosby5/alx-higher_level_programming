f069376859d46d8930e9279a6cb423355bedd70e
Pcosby5 <pcosby50@gmail.com>
2023-10-08 22:36:51 +0000

    10-divisible_by_2.py

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -1,4 +1,18 @@
 #!/usr/bin/python3
 
 def divisible_by_2(my_list=[]):
-    return [not i & 1 for i in my_list]
+    # Create an empty list to store the results
+    results = []
+
+    # Iterate through the elements in the input list
+    for number in my_list:
+        # Check if the current number is divisible by 2 (even)
+        if number % 2 == 0:
+            # If divisible by 2, append True to the results list
+            results.append(True)
+        else:
+            # If not divisible by 2, append False to the results list
+            results.append(False)
+
+    # Return the list of True and False values
+    return results
