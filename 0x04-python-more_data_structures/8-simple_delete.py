5b820e15599bdf1b187a7b5c1060bb6cccac65db
Pcosby5 <pcosby50@gmail.com>
2023-10-11 19:44:10 +0000

    8-simple_delete.py

---
+++ 0x04-python-more_data_structures/8-simple_delete.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+def simple_delete(a_dictionary, key=""):
+    # Check if the key exists in the dictionary
+    if key in a_dictionary:
+        # If the key exists, delete it using the del statement
+        del a_dictionary[key]
