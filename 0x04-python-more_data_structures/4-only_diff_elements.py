f99c3d2825409cee0e2f60842e421ab76dce06c1
Pcosby5 <pcosby50@gmail.com>
2023-10-11 19:06:59 +0000

    4-only_diff_elements.py

--- 0x04-python-more_data_structures/4-only_diff_elements.py
+++ 0x04-python-more_data_structures/4-only_diff_elements.py
@@ -1,4 +1,24 @@
 #!/usr/bin/python3
 def only_diff_elements(set_1, set_2):
-    # Use the symmetric difference operator (^)
-     return set_1 ^ set_2
+    # Create empty sets to store elements present in only one of the sets
+    unique_to_set_1 = set()
+    unique_to_set_2 = set()
+
+    # Iterate through the elements in set_1
+    for item in set_1:
+        # Check if the item is not in set_2
+        if item not in set_2:
+            # If it's only in set_1, add it to unique_to_set_1
+            unique_to_set_1.add(item)
+
+    # Iterate through the elements in set_2
+    for item in set_2:
+        # Check if the item is not in set_1
+        if item not in set_1:
+            # If it's only in set_2, add it to unique_to_set_2
+            unique_to_set_2.add(item)
+
+    # Combine the elements that are unique to each set
+    only_diff_elements_set = unique_to_set_1.union(unique_to_set_2)
+
+    return only_diff_elements_set
