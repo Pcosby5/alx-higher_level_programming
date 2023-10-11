9c1c7aa13e77300a034b0173e2d1af834773c572
Pcosby5 <pcosby50@gmail.com>
2023-10-11 19:48:57 +0000

    8-simple_delete.py

--- 0x04-python-more_data_structures/8-simple_delete.py
+++ 0x04-python-more_data_structures/8-simple_delete.py
@@ -2,7 +2,5 @@
 
 def simple_delete(a_dictionary, key=""):
     # Use the pop method with a default value of None to safely delete the key
-    # If the key doesn't exist, no error is raised, and the dictionary remains unchanged
     a_dictionary.pop(key, None)
-
     return a_dictionary
