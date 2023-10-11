763af3a78cd89de64f71cf6304d63e29a5f8463d
Pcosby5 <pcosby50@gmail.com>
2023-10-11 18:14:25 +0000

    0-square_matrix_simple.py

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,4 +1,3 @@
 #!/usr/bin/python3
-
 def square_matrix_simple(matrix=[]):
-    return [list(map(lambda func: func ** 2, k)) for k in matrix]
+    return [list(map(lambda x: x ** 2, y)) for y in matrix]
