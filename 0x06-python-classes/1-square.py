199d9c8294dd7f81245edabf28cff875561085ae
Pcosby5 <pcosby50@gmail.com>
2023-10-24 16:49:55 +0000

    1-square.py

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 """
-This module defines a Square class and initialises it size
+This module defines a Square class and initialise its size
 """
 
 
 class Square:
-    """Implementation of square 
+    """Square implementation
     """
 
     def __init__(self, size):
