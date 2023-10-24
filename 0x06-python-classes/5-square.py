aa17103d86a33ba7512a96c41c1d194a583ff0db
Pcosby5 <pcosby50@gmail.com>
2023-10-24 17:48:00 +0000

    5-square.py

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+"""
+This module defines a Square class
+
+It implements value and type checks for its attributes with area function
+"""
+
+
+class Square:
+    """Square implementation
+    """
+    def __init__(self, size=0):
+        self.size = size
+
+    @property
+    def size(self):
+        """Retreives size"""
+        return self.__size
+
+    @size.setter
+    def size(self, size):
+        """Checks size and sets it"""
+        if type(size) != int:
+            raise TypeError('size must be an integer')
+        elif size < 0:
+            raise ValueError('size must be >= 0')
+        self.__size = size
+
+    def area(self):
+        """Implements area"""
+        return (self.__size ** 2)
+
+    def my_print(self):
+        if self.__size == 0:
+            print('')
+        else:
+            for _ in range(self.__size):
+                print("#" * self.__size)
