44038175e389bcb04a978779358e8ead7e808f67
Pcosby5 <pcosby50@gmail.com>
2023-11-08 18:02:22 +0000

    0-read_file.py

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,11 +1,15 @@
 #!/usr/bin/python3
-"""read a file"""
+"""
+0-read_file module
+"""
 
 
 def read_file(filename=""):
-    """read a file"""
-    try:
-        with open(filename, mode="r", encoding="utf-8") as file:
-            print(file.read(), end="")
-    except FileNotFoundError:
-        pass
+    """
+    read_file - reads a text file (UTF8) and prints it to stdout
+    Args:
+        filename: name of the file
+    """
+    with open(filename, "r", encoding="UTF-8") as file:
+        for line in file:
+            print(line, end="")
