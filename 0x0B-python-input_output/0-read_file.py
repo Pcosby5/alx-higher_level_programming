42a7053c704076dd23d7f7219f1611e67119de4c
Pcosby5 <pcosby50@gmail.com>
2023-11-08 18:04:38 +0000

    0-read_file.py

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-0-read_file module
+0-read_file
 """
 
 
@@ -8,8 +8,8 @@ def read_file(filename=""):
     """
     read_file - reads a text file (UTF8) and prints it to stdout
     Args:
-        filename: name of the file
+        filename: name of file
     """
-    with open(filename, "r", encoding="UTF-8") as file:
-        for line in file:
+    with open(filename, "r", encoding="UTF-8") as f:
+        for line in f:
             print(line, end="")
