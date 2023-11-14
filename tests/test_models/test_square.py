55bc9b4806313608b630bd8142bd225b03196745
Pcosby5 <pcosby50@gmail.com>
2023-11-14 04:06:00 +0000

    tests

---
+++ tests/test_models/test_square.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""Unittests for base
+"""
+
+import unittest
+from models.square import Square
+
+
+class TestSquare(unittest.TestCase):
+    """Define unittest for Square models"""
+
+    def test_initialization_success(self):
+        sq1 = Square(5)
+        sq2 = Square(10)
+        self.assertEqual(sq1.id, 5)
+        self.assertEqual(sq2.id, 6)
+
+    def test_initialization_without_arguments(self):
+
+        self.assertRaises(TypeError, Square)
+
+if __name__ == '__main__':
+    unittest.main()
