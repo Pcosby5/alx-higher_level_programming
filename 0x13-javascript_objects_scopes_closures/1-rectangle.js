5d4c452fa6a90c96a16332e8a48ee3399e144cf8
Pcosby5 <pcosby50@gmail.com>
2024-01-09 21:27:32 +0100

    1-rectangle.js

--- 0x13-javascript_objects_scopes_closures/1-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/1-rectangle.js
@@ -2,7 +2,7 @@
 
 // Define a class named Rectangle
 class Rectangle {
-  constructor(w, h) {
+  constructor (w, h) {
     this.width = w; // Initialize width with the value of w
     this.height = h; // Initialize height with the value of h
   }
