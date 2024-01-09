5ee793d895cc833a59bcdb52785804e098672de8
Pcosby5 <pcosby50@gmail.com>
2024-01-09 04:39:50 +0100

    13-add.js

---
+++ 0x12-javascript-warm_up/13-add.js
@@ -0,0 +1,6 @@
+#!/usr/bin/node
+
+// Define a function called add and use  exports to make it accessible outside the script
+exports.add = function (a, b) {
+  return a + b; // Return the addition of the two integers passed as arguments
+};
