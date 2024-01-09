7b287af79bde0e2e7c2a0f13d83b581e8394b883
Pcosby5 <pcosby50@gmail.com>
2024-01-09 21:12:38 +0100

    0-rectangle.js

---
+++ 0x13-javascript_objects_scopes_closures/0-rectangle.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+
+// Define a class named Rectangle
+class Rectangle {
+  // Definition of an empty class
+}
+
+module.exports = Rectangle; // Export the Rectangle class to make it accessible outside the script
