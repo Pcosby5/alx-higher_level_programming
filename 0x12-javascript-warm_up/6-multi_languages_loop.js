43ae066d2b23b30fcd3e2e84fcc02f56f9c9600a
Pcosby5 <pcosby50@gmail.com>
2024-01-09 01:48:56 +0100

    6-multi_languages_loop.js

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+// Script that print 3lines using an array of string and loop
+const array = [
+  'C is fun',
+  'Python is cool',
+  'JavaScript is amazing'
+];
+
+for (let i = 0; i < array.length; i++) {
+  console.log(array[i]);
+}
