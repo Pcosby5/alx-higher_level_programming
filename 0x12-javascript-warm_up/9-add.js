1632961d3557469b6fd8f7ee8209e4e920f03368
Pcosby5 <pcosby50@gmail.com>
2024-01-09 02:17:38 +0100

    9-add.js

---
+++ 0x12-javascript-warm_up/9-add.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+
+// Define the function add(a, b) to add two integers
+function add(a, b) {
+  return a + b; // Return the sum of the two integers
+}
+
+// Call the add function with the integers parsed from arguments and print the result
+console.log(add(parseInt(process.argv[2]), parseInt(process.argv[3])));
