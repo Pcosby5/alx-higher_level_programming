dc2979d5ca82a3b8332f3c8cb8e8d4a624cd263e
Pcosby5 <pcosby50@gmail.com>
2024-01-09 21:24:51 +0100

    1-rectangle.js

---
+++ 0x13-javascript_objects_scopes_closures/1-rectangle.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+// Define a class named Rectangle
+class Rectangle {
+  constructor(w, h) {
+    this.width = w; // Initialize width with the value of w
+    this.height = h; // Initialize height with the value of h
+  }
+}
+
+module.exports = Rectangle; // Export the Rectangle class to make it accessible outside the script
