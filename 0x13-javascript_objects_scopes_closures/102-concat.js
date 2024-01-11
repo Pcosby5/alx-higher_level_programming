5a5d1b33b45b46f3d6d704ed24785f71d8612adc
Pcosby5 <pcosby50@gmail.com>
2024-01-11 02:11:34 +0100

    102-concat.js

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -1,26 +1,15 @@
 #!/usr/bin/node
 
-const fs = require('fs');
+const file = require('fs'); // Import the 'fs' module for file operations
 
-// Ensure that three command-line arguments are provided
-if (process.argv.length !== 5) {
-  console.error('Usage: ./concatFiles.js <file1> <file2> <destination>');
-  process.exit(1);
-}
+// Read the contents of the first source file (specified in the first command-line argument)
+const fileA = file.readFileSync(process.argv[2], 'utf8');
 
-// Extract file paths from command-line arguments
-const [, , sourceFilePath1, sourceFilePath2, destinationFilePath] = process.argv;
-
-// Read the contents of the first source file
-const content1 = fs.readFileSync(sourceFilePath1, 'utf-8');
-
-// Read the contents of the second source file
-const content2 = fs.readFileSync(sourceFilePath2, 'utf-8');
+// Read the contents of the second source file (specified in the second command-line argument)
+const fileB = file.readFileSync(process.argv[3], 'utf8');
 
 // Concatenate the contents of the two files
-const concatenatedContent = content1 + content2;
+const concatenatedContent = fileA + fileB;
 
-// Write the concatenated content to the destination file
-fs.writeFileSync(destinationFilePath, concatenatedContent);
-
-console.log(`Contents of ${sourceFilePath1} and ${sourceFilePath2} concatenated to ${destinationFilePath}`);
+// Write the concatenated content to the destination file (specified in the third command-line argument)
+file.writeFileSync(process.argv[4], concatenatedContent);
