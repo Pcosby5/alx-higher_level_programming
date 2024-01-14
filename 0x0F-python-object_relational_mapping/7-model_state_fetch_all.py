db2247bb3657bec9f01c430168a3343b24bd46f3
Pcosby5 <pcosby50@gmail.com>
2024-01-14 11:49:11 +0100

    7-model_state_fetch_all.py

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -1,16 +1,17 @@
 #!/usr/bin/python3
 """List all states"""
 
-import sys
+from sys import argv
+from model_state import State, Base
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from model_state import Base, State
+
 
 if __name__ == "__main__":
-    # Create a SQLAlchemy engine to connect to the database
+    """Access to db and get States from database"""
     engine = create_engine(
         'mysql+mysqldb://{}:{}@localhost/{}'
-        .format(sys.argv[1], sys.argv[2], sys.argv[3]),
+        .format(argv[1], argv[2], argv[3]),
         pool_pre_ping=True
     )
 
