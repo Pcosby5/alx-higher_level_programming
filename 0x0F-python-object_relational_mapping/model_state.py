06bada9a7d415c2dca9190ba2340b0fcc183b50f
Pcosby5 <pcosby50@gmail.com>
2024-01-14 11:37:17 +0100

    model_state.py

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -2,9 +2,7 @@
 """Lists states"""
 
 from sqlalchemy import Column, Integer, String
-from sqlalchemy.orm import relationship
 from sqlalchemy.ext.declarative import declarative_base
-from model_city import City
 
 Base = declarative_base()
 
