27af65e7c4c2bd2a510fb06a531e73a03dcca3ba
Pcosby5 <pcosby50@gmail.com>
2024-02-29 10:51:20 -0800

    100-script.js

--- 0x15-javascript-web_jquery/100-script.js
+++ 0x15-javascript-web_jquery/100-script.js
@@ -1,8 +1,9 @@
 // Ensures that the script is exec only after DOM has fully loaded
 document.addEventListener("DOMContentLoaded", function () {
     // Select the header element
-    var headerElement = document.querySelector('header');
+    let headerElement = document.querySelector('header');
 
     // Update the text color to red
     headerElement.style.color = '#FF0000';
 });
+
